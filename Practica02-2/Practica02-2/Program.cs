06778a333e0c89a5ed18c41474a637d2ef3a8fc0
asinusware <danielzone46@hotmail.com>
2015-04-01 00:32:40 -0600

    Clase Cronometro
    Se crea una clase Cronometro con interfaz publica y privada

--- Practica02-2/Practica02-2/Program.cs
+++ Practica02-2/Practica02-2/Program.cs
@@ -7,38 +7,68 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Timers;
+using System.Text;  
 using System.Threading;
 
 namespace Practica02_2
 {
+    class Cronometro
+    {
+        private  DateTime tiempo;
+        private const int resolucion = 10;
+        private int x; //Columna en pantalla
+        private int y; //Fila en pantalla
+        public bool Iniciado;
+
+        public Cronometro()
+        {
+            tiempo = new DateTime(0);
+            Iniciado = false;
+            x = 0;
+            y = 0;
+        }
+
+        private void Mostrar()
+        {
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+            Console.Write(tiempo.Minute.ToString("00") + ":" + tiempo.Second.ToString("00") + "." + (tiempo.Millisecond/resolucion).ToString("00"));
+        }
+
+        public void Iniciar()
+        {
+            Iniciado = true;
+            x = Console.CursorLeft;
+            y = Console.CursorTop;
+            do
+            {
+                Mostrar();
+                Thread.Sleep(10);
+                tiempo = tiempo.AddMilliseconds(10);
+            }
+            while ((tiempo.Minute < 1) || Iniciado == false);
+        }
+
+    }
+
     public class AppCronometro
     {
 
         public static void Main()
         {
-            DateTime tiempo = new DateTime(0);
-            DateTime tiempo3;
-            DateTime tiempo2 = DateTime.Now;
-            do
-            {
-                Console.Clear();
-                Console.WriteLine(tiempo.Minute.ToString() + ":" + tiempo.Second + "." + tiempo.Millisecond);
-                Thread.Sleep(100);
-                tiempo = tiempo.AddMilliseconds(100);
-            }
-            while (tiempo.Second < 30);
-            tiempo3 = DateTime.Now;
-            Console.WriteLine(tiempo2.ToLongTimeString() + " " + tiempo3.ToLongTimeString() );
+            Cronometro miCronometro = new Cronometro();
+            DateTime sonda;
+
+            Console.Clear();
+            Console.WriteLine("Cronometro para consola. asinusware\u00a9 2015");
+            Console.WriteLine("[I]niciar [D]etener [S]alir \n");
+            sonda = DateTime.Now;
+            Console.WriteLine("Cronometro inicia a: " + sonda.ToLongTimeString());
+            Console.CursorVisible = false;
+            miCronometro.Iniciar();
+            sonda = DateTime.Now;
+            Console.WriteLine("\nCronometro Termina a: " + sonda.ToLongTimeString() );
             Console.ReadLine();
         }
-
-        /*private static void avanzar(object source, ElapsedEventArgs e)
-        {
-            tiempo.AddMilliseconds(10);
-            Console.WriteLine(tiempo.ToLongTimeString());
-        }
-        */
     }
 }
